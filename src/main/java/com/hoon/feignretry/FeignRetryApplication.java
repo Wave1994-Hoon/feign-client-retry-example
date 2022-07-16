ebc0e7f5157958e4e6178645878129a7218a6c34
Wave1994-Hoon <kk03187693@gmail.com>
2022-07-16 22:52:50 +0900

    init

---
+++ src/main/java/com/hoon/feignretry/FeignRetryApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.feignretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignRetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoon/feignretry/FeignRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.feignretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
